d7ea5b4c165d83d4f75be0e2f5b3da89b477f6c9
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-07-29 23:42:50 +0530

    Freelancing section removed

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   //Copy right year
   $("#copyright-year").text(new Date().getFullYear());
-
-  const startDate = new Date("2020-01-05"); // Start date in YYYY-MM-DD format
+    //2020-01-05 [Freelance start date]
+  const startDate = new Date("2021-07-05"); // Start date in YYYY-MM-DD format
 
   function calculateExperience() {
     const endDate = new Date();
